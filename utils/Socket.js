f6050d456f5a89380a5df4e6ed1edef7599d6310
IzaanArain <izaansaquib@gmail.com>
2023-11-08 23:41:22 +0500

    updated

--- utils/Socket.js
+++ utils/Socket.js
@@ -6,7 +6,7 @@ const mongoose = require("mongoose");
 const socket = async (io) => {
   try {
     io.on("connection", (socket) => {
-      console.log("user connected", socket.id);
+      // console.log("user connected", socket.id);
       // socket.emit("serverConnected", "Server is connected");
 
       socket.on("get_all_messages", async (data) => {
