33e5249bc67dc4e847821a1322d045160731e17f
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-11-13 21:05:32 +0500

    updated

--- server.js
+++ server.js
@@ -15,7 +15,12 @@ const app = express();
 app.use(cors());
 const server = http.createServer(app);
 const { Server } = require("socket.io");
-const io = new Server(server);
+const io = new Server(server,{
+  cors: {
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  },
+});
 socket(io);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
