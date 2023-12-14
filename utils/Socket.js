7e99641a55b42d694f845e593e8ab32ef51e0c6a
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-12-14 20:48:47 +0500

    first commit

--- utils/Socket.js
+++ utils/Socket.js
@@ -7,13 +7,10 @@ const socket = async (io) => {
   try {
 
     io.on("connection", (socket) => {
-      // console.log("user connected", socket.id);
-      // socket.emit("serverConnected", "Server is connected");
-
+     
       socket.on("get_all_messages", async (data) => {
         try {
           const { sender_id, receiver_id } = data;
-          // console.log(data)
           if (!sender_id || !receiver_id) {
             throw new Error("Please provide both sender_id and receiver_id.");
           } else if (
@@ -31,7 +28,6 @@ const socket = async (io) => {
           } else if (!receiver) {
             throw new Error("Receiver not found.");
           }
-          // const room = `room${sender_id}${receiver_id}`;
           const room = `room${[sender_id, receiver_id].sort().join("")}`;
           socket.join(room);
           const chat_messages = await Message.find({
@@ -48,11 +44,8 @@ const socket = async (io) => {
             //   sender_id: new mongoose.Types.ObjectId(sender_id),
             //   receiver_id: new mongoose.Types.ObjectId(receiver_id),
           });
-          // console.log(chat_messages.length);
+          console.log(chat_messages.length);
           if (chat_messages.length > 0) {
-            // console.log("get_all_messages : ", room);
-            // console.log("room created", socket.rooms);
-            // socket.emit("get_all_messages", chat_messages);
             io.to(room).emit("get_all_messages", chat_messages);
           } else {
             io.to(room).emit("get_all_messages", []);
@@ -66,7 +59,7 @@ const socket = async (io) => {
       socket.on("send_message", async (data) => {
         try {
           const { sender_id, receiver_id, message: typed_message } = data;
-
+          // console.log({ sender_id, receiver_id, message: typed_message })
           if (!sender_id) {
             throw new Error("please enter sender_id");
           } else if (!receiver_id) {
@@ -96,11 +89,11 @@ const socket = async (io) => {
             time: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
           });
           const new_message = await message.save();
-          // console.log(new_message)
+          // console.log("message created",new_message)
           socket.to(room).emit("receive_message", new_message);
           // io.to(room).emit("receive_message", new_message);
         } catch (err) {
-          // console.error("Error", err.message);
+          console.error("Error", err.message);
           socket.emit("error_message", err.message);
         }
       });
