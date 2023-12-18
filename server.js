6fc794147a55ad1425e807ee2fed829f1cd52dd3
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2023-12-18 17:16:08 +0500

    updated

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const http = require("http");
 const cors = require("cors");
 const socket=require("./utils/Socket");
 const newSocket=require("./utils/NewSocket");
+const ChatSocket=require("./utils/ChatSocket")
 const app = express();
 app.use(cors());
 const server = http.createServer(app);
@@ -20,7 +21,8 @@ const io = new Server(server,{
   },
 });
 // socket(io);
-newSocket(io);
+// newSocket(io);
+ChatSocket(io);
 app.use(express.json());                              
 app.use(express.urlencoded({ extended: false }));
 // app.use(requstDetails)
---
+++ utils/ChatSocket.js
@@ -0,0 +1,102 @@
+const Message = require("../models/MessageModel");
+const User = require("../models/UserModel");
+const moment = require("moment");
+const mongoose = require("mongoose");
+
+const socket = async (io) => {
+  try {
+
+    io.on("connection", (socket) => {
+      console.log("user connected", socket.id);
+      
+      socket.on("get_all_messages", async (data) => {
+        try {
+          const { sender_id, receiver_id } = data;
+          if (!sender_id || !receiver_id) {
+            throw new Error("Please provide both sender_id and receiver_id.");
+          } else if (
+            !mongoose.isValidObjectId(sender_id) ||
+            !mongoose.isValidObjectId(receiver_id)
+          ) {
+            throw new Error("Invalid sender_id or receiver_id.");
+          }
+          const [sender, receiver] = await Promise.all([
+            User.findById(sender_id),
+            User.findById(receiver_id),
+          ]);
+          if (!sender) {
+            throw new Error("Sender not found.");
+          } else if (!receiver) {
+            throw new Error("Receiver not found.");
+          }
+          const room = `room${[sender_id, receiver_id].sort().join("")}`;
+          socket.join(room);
+          const chat_messages = await Message.find({
+            $or: [
+              {
+                sender_id:sender_id,
+                receiver_id:receiver_id,
+              },
+              {
+                sender_id:receiver_id,
+                receiver_id:sender_id,
+              },
+            ],
+          });
+          if (chat_messages.length > 0) {
+            io.to(room).emit("get_all_messages", chat_messages);
+          } else {
+            io.to(room).emit("get_all_messages", []);
+          }
+        } catch (err) {
+          console.error("Error", err.message);
+          socket.emit("error_message", err.message);
+        }
+      });
+
+      socket.on("send_message", async (data) => {
+        try {
+          const { sender_id, receiver_id, message: typed_message } = data;
+          if (!sender_id) {
+            throw new Error("please enter sender_id");
+          } else if (!receiver_id) {
+            throw new Error("please receiver_id");
+          } else if (!mongoose.isValidObjectId(sender_id)) {
+            throw new Error("Invalid sender_id");
+          } else if (!mongoose.isValidObjectId(receiver_id)) {
+            throw new Error("Invalid receiver_id");
+          }
+          const sender = await User.findById(sender_id);
+          if (!sender) {
+            throw new Error("sender not found");
+          }
+          const receiver = await User.findById(receiver_id);
+          if (!receiver) {
+            throw new Error("receiver not found");
+          }
+          const room = `room${[sender_id, receiver_id].sort().join("")}`;
+          const message = new Message({
+            sender_id: sender_id,
+            receiver_id: receiver_id,
+            message: typed_message,
+            time: moment(Date.now()).format("MMMM Do YYYY, h:mm:ss a"),
+          });
+          const new_message = await message.save();
+          socket.to(room).emit("receive_message", new_message);
+          // io.to(room).emit("receive_message", new_message);
+        } catch (err) {
+          console.error("Error", err.message);
+          socket.emit("error_message", err.message);
+        }
+      });
+
+      socket.on("disconnect", () => {
+        console.log("A user disconnected", socket.id);
+      });
+    });
+  } catch (err) {
+    console.error("Error", err.message);
+  }
+};
+
+module.exports=socket;
