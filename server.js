8b7734e5e5d12a316651ad061cc3a46bbb3b581e
Muhammad Izzan Saqib <izzan.saqib@technado.pk>
2024-02-02 19:20:55 +0500

    updated

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ const { Server } = require("socket.io");
 const { requstDetails } = require("./middleware/RequestDetails");
 const io = new Server(server,{
   cors: {
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE"],
   },
 });
